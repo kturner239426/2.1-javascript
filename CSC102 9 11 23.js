8932b31f97c2aad88c8a8e34615740b22048bb22
kturner239426 <144501289+kturner239426@users.noreply.github.com>
2023-09-17 15:27:18 -0400

    CSC102 9 11 23.js

--- CSC102 9 11 23.js
+++ CSC102 9 11 23.js
@@ -1,6 +1,8 @@
 /* Kayla Turner functions for wk 2*/
 function funcAlert()
 {
+    /* variable function for two random #'s*/
+    /* one sum function for the sum of var die1 and die2*/
     alert("you pushed the button on 9 11 23");
     var die1 = Math.ceil(Math.random()*6)
     var die2 = Math.ceil(Math.random()*6)
@@ -12,12 +14,12 @@ function funcAlert()
     {
     alert("die1 = " + die1 + " you lose!");
     }
-
+/* if the sum  of die 1 and die2 is equal to 7 the player loses*/
    else if (sum ==7)
     {
         alert("YOU LOSE!")
     }
-
+/*if the sum of die1 and die 2 is equal to 11 the player loses*/
     else if (sum ==11)
     {
         alert("YOU LOSE!")
@@ -25,6 +27,7 @@ function funcAlert()
 
     else
     {
+        /*if the sume of die1 and die2 is an even # the player wins*/
         alert("die1 = " + die1 + " you won!"); 
     }
 } 
